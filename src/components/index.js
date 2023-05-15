8a0c4e6c5906a720a2025ba222a39dc2d7667bcd
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2023-05-15 17:40:20 +0530

    api call with redux toolkit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SongCard from "./SongCard";
+
+import MusicPlayer from "./MusicPlayer";
+import Loader from "./Loader";
+import Error from "./Error";
+
+export { SongCard, MusicPlayer, Loader, Error };
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'; // utility function from redux toolkit for api call
+
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "f08119ec51mshd434135a9890db4p1c84fdjsn6a7df6a8fbe1",
+//     "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+//   },
+// };
+
+// fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+// ****API CALL IN REDUX TOOLKIT**** //
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        `${import.meta.env.VITE_RAPID_API_KEY}`,
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
+//  to use this api end point when call is made .
